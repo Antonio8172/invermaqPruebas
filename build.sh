5237329e72bf2d4f8272bd4041df18ec33bdf822
Antonio8172 <113269761+Antonio8172@users.noreply.github.com>
2025-04-07 23:19:21 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+gunicorn MaqmanSite.wsgi:application --bind 0.0.0.0:$PORT
